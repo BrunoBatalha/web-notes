f3fd2829c5f958f6933dea72022329cdf79d7343
Bruno Batalha <brunobatalha37@gmail.com>
2020-05-06 21:30:04 -0400

    Altera nome do site

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     render() {
         return (
             <div className='container'>
-                <h1 className='text-center pt-3'>AnotaWeb</h1>
+                <h1 className='text-center pt-3'>Web Notes</h1>
                 <div className='row'>
                     <Form />
                 </div>
